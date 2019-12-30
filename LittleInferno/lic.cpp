4316fc259eb66965354fd9e37de5fc57c44c7cbd
Robin Hack <hack.robin@gmail.com>
2019-12-30 19:41:02 +0100

    Little Inferno: Unlimited money patch for macOS binary.

---
+++ LittleInferno/lic.cpp
@@ -0,0 +1,29 @@
+// Compatible file (Epic Store):
+// $ shasum ./Little\ Inferno
+// 40263e6aef42d26c8821817bdcfa971e5df18fe5  ./Little Inferno
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    fstream victim;
+
+    victim.open("Little Inferno",  ios::out | ios::in | ios::binary);
+
+    // Thank you Hop*er.
+    // Function name: ProfileGetMoney(eItemMoneyType)
+    victim.seekp(0x058c60);
+
+    // this leads to unlimited money and coupons :)
+    // mov rax, 0xffff
+    const unsigned char magic_bytes[5] = { 0xb8, 0xff, 0xff, 0x00, 0x00 }; // 0xb8, 0x9f, 0x86, 0x01, 0x00, 0x90};
+    victim.write((char *) &magic_bytes, sizeof(magic_bytes));
+
+    victim.close();
+
+    cout << "You... human.. has been h4cked! Enjoy the best game!" << endl;
+    return 0;
+}
